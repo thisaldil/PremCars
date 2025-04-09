c607973fdc6e69cc476cde4c16f1952e9e6e7f51
thisaldil <tdimith10@gmail.com>
2025-04-09 13:14:04 +0530

    finit

--- server/server.js
+++ server/server.js
@@ -41,5 +41,5 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
