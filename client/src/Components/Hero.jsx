e5f4f91946f189a7efa5f86aaf8398f2a76f2756
thisaldil <tdimith10@gmail.com>
2025-05-14 22:30:23 +0530

    Update Hero.jsx

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -50,14 +50,14 @@ const Hero = ({ onBookNowClick }) => {
       <div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
         <div>
           <a
-            title="Find Us"
+            title="Srilanka"
             href="https://www.instagram.com/dr.notorio.us/"
             target="_blank"
             className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12"
           >
             <img
               className="object-cover object-center w-full h-full rounded-full"
-              src="https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg"
+              src="https://th.bing.com/th/id/OIP.hmH-N9qvexNOG3st84TGxQHaGD?cb=iwp2&w=1920&h=1571&rs=1&pid=ImgDetMain"
               alt="Buy me a beer"
             />
           </a>
